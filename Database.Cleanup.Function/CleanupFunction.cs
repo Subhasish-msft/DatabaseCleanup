cb9c2115fa0cd46a6aa90ddd3ca21d48fd8c5e07
Subhasish Pani <v-subass@microsoft.com>
2020-02-14 15:35:09 +0530

    Added logic for Http POST call to apps

--- Database.Cleanup.Function/CleanupFunction.cs
+++ Database.Cleanup.Function/CleanupFunction.cs
@@ -1,4 +1,5 @@
 using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Net.Http;
@@ -11,47 +12,70 @@ namespace Database.Cleanup.Function
     public static class CleanupFunction
     {
         [FunctionName("CleanupFunction")]
-        public static async void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        public static async void Run([TimerTrigger("0 0 9 * * MON")]TimerInfo myTimer, ILogger log, ExecutionContext context) 
         {
+            // (0 0 9 * * MON) == every mondaty 9 am  
+            // Use of debug (0 */1 * * * *) == every minute
             log.LogInformation($"C# Timer trigger function Started at: {DateTime.Now}");
-            await HandleCleanup();
+            await HandleCleanup(log, context);
             log.LogInformation($"C# Timer trigger function Ended at: {DateTime.Now}");
         }
 
-        private static async Task HandleCleanup()
+        private static async Task HandleCleanup(ILogger log, ExecutionContext context)
         {
-            try
+            var config = new ConfigurationBuilder()
+                       .SetBasePath(context.FunctionAppDirectory)
+                       .AddJsonFile("local.settings.json", optional: false, reloadOnChange: true)
+                       .AddEnvironmentVariables()
+                       .Build();
+            var appDetails = config["AppDetails"];
+            var authKey = config["AuthKey"];
+            if (string.IsNullOrEmpty(appDetails))
             {
-                var appsettingList = System.Environment.GetEnvironmentVariable("AppDetails");
-                var accessToken = await GetAccessTokenAsync();
-                var body = string.Empty;
-                using (var client = new HttpClient())
+                log.LogError($"AppDetails is empty");
+                return;
+            }
+            if (string.IsNullOrEmpty(authKey))
+            {
+                log.LogError($"AuthKey is empty");
+                return;
+            }
+
+            var appUrls = appDetails.Split(';');
+            foreach (var url in appUrls)
+            {
+                try
                 {
-                    using (var request = new HttpRequestMessage(HttpMethod.Post, appsettingList))
+                    var body = string.Empty;
+                    using (var client = new HttpClient())
                     {
-                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-                        request.Content = new StringContent(body, Encoding.UTF8, "application/json");
-                        using (HttpResponseMessage response = await client.SendAsync(request))
+                        using (var request = new HttpRequestMessage(HttpMethod.Post, url))
                         {
-                            if (response.IsSuccessStatusCode)
+                            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                            request.Headers.Add("AuthKey", authKey);
+                            request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+                            using (HttpResponseMessage response = await client.SendAsync(request))
                             {
-
+                                if (response.IsSuccessStatusCode)
+                                {
+                                    log.LogInformation($"Cleanup successful, URL: {url}");
+                                }
+                                else
+                                {
+                                    log.LogError($"Cleanup unsuccessful, URL: {url} " +
+                                        $"status code: {response.StatusCode} ," +
+                                        $"Error Message: {response.RequestMessage}");
+                                }
                             }
-
                         }
                     }
                 }
+                catch (Exception ex)
+                {
+                    log.LogError($"Exception ocured during Cleanup. URL: {url} ," +
+                        $"Exception message: {ex.Message}");
+                }
             }
-            catch (Exception ex)
-            {
-                throw;
-            }
-        }
-
-        public static async Task<string> GetAccessTokenAsync()
-        {
-            return string.Empty;
         }
     }
 }
