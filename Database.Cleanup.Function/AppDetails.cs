16249ff10a2e4ba0c3ba4cfa9232dd5dd5baa32b
Subhasish Pani <v-subass@microsoft.com>
2020-02-17 11:59:57 +0530

    Added appsettings.json file

---
+++ Database.Cleanup.Function/AppDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Cleanup.Function
+{
+    public class AppDetails
+    {
+        public string AppName { get; set; }
+        public string Url { get; set; }
+        public string AuthKey { get; set; }
+    }
+    public class AppDetil {
+        public AppDetails[] AppDetails { get; set; }
+    }
+}
--- Database.Cleanup.Function/CleanupFunction.cs
+++ Database.Cleanup.Function/CleanupFunction.cs
@@ -1,7 +1,8 @@
 using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -12,57 +13,53 @@ namespace Database.Cleanup.Function
     public static class CleanupFunction
     {
         [FunctionName("CleanupFunction")]
-        public static async void Run([TimerTrigger("0 0 9 * * MON")]TimerInfo myTimer, ILogger log, ExecutionContext context) 
+        public static async void Run([TimerTrigger("0 0 9 * * MON")]TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             // (0 0 9 * * MON) == every mondaty 9 am  
             // Use of debug (0 */1 * * * *) == every minute
-            log.LogInformation($"C# Timer trigger function Started at: {DateTime.Now}");
+            log.LogInformation($"Cleanup Function Started at: {DateTime.Now}");
             await HandleCleanup(log, context);
-            log.LogInformation($"C# Timer trigger function Ended at: {DateTime.Now}");
+            log.LogInformation($"Cleanup Function Ended at: {DateTime.Now}");
         }
 
         private static async Task HandleCleanup(ILogger log, ExecutionContext context)
         {
-            var config = new ConfigurationBuilder()
-                       .SetBasePath(context.FunctionAppDirectory)
-                       .AddJsonFile("local.settings.json", optional: false, reloadOnChange: true)
-                       .AddEnvironmentVariables()
-                       .Build();
-            var appDetails = config["AppDetails"];
-            var authKey = config["AuthKey"];
-            if (string.IsNullOrEmpty(appDetails))
+            var jsonObj = File.ReadAllText(Path.Combine(context.FunctionAppDirectory, @"appsettings.json"));
+            var appDetails = JsonConvert.DeserializeObject<AppDetil>(jsonObj);
+            if (appDetails == null || appDetails.AppDetails.Length == 0)
             {
-                log.LogError($"AppDetails is empty");
-                return;
-            }
-            if (string.IsNullOrEmpty(authKey))
-            {
-                log.LogError($"AuthKey is empty");
+                log.LogError($"No apps found in config for cleanup");
                 return;
             }
 
-            var appUrls = appDetails.Split(';');
-            foreach (var url in appUrls)
+            foreach (var app in appDetails.AppDetails)
             {
+
+                if (string.IsNullOrEmpty(app.AuthKey))
+                {
+                    log.LogError($"AuthKey is empty for App { app.AppName}");
+                    return;
+                }
+
                 try
                 {
                     var body = string.Empty;
                     using (var client = new HttpClient())
                     {
-                        using (var request = new HttpRequestMessage(HttpMethod.Post, url))
+                        using (var request = new HttpRequestMessage(HttpMethod.Post, app.Url))
                         {
                             request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                            request.Headers.Add("AuthKey", authKey);
+                            request.Headers.Add("AuthKey", app.AuthKey);
                             request.Content = new StringContent(body, Encoding.UTF8, "application/json");
                             using (HttpResponseMessage response = await client.SendAsync(request))
                             {
                                 if (response.IsSuccessStatusCode)
                                 {
-                                    log.LogInformation($"Cleanup successful, URL: {url}");
+                                    log.LogInformation($"Cleanup successful. App Name:{app.AppName}, URL: {app.Url}");
                                 }
                                 else
                                 {
-                                    log.LogError($"Cleanup unsuccessful, URL: {url} " +
+                                    log.LogError($"Cleanup unsuccessful. App Name:{app.AppName}, URL: {app.Url} " +
                                         $"status code: {response.StatusCode} ," +
                                         $"Error Message: {response.RequestMessage}");
                                 }
@@ -72,7 +69,7 @@ namespace Database.Cleanup.Function
                 }
                 catch (Exception ex)
                 {
-                    log.LogError($"Exception ocured during Cleanup. URL: {url} ," +
+                    log.LogError($"Exception ocured during Cleanup. App Name:{app.AppName}, URL: {app.Url} ," +
                         $"Exception message: {ex.Message}");
                 }
             }
